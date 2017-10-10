4097ff0ae751decf8516846a33cb56e10007a357
raito2043 <1015881481@qq.com>
2017-10-11 02:25:06 +0800

    P2PInvest 注册 （待处理：小米本地头像问题）  10110224

---
+++ app/src/main/java/com/example/raito/p2pinvest/activity/RegisterActivity.java
@@ -0,0 +1,171 @@
+package com.example.raito.p2pinvest.activity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.alibaba.fastjson.JSON;
+import com.example.raito.p2pinvest.R;
+import com.example.raito.p2pinvest.common.AppNetConfig;
+import com.example.raito.p2pinvest.common.BaseActivity;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class RegisterActivity extends BaseActivity {
+
+
+    @BindView(R.id.img_pre)
+    ImageView imgPre;
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.img_setting)
+    ImageView imgSetting;
+    @BindView(R.id.et_register_number)
+    EditText etRegisterNumber;
+    @BindView(R.id.et_register_name)
+    EditText etRegisterName;
+    @BindView(R.id.et_register_pwd)
+    EditText etRegisterPwd;
+    @BindView(R.id.et_register_pwdagain)
+    EditText etRegisterPwdagain;
+    @BindView(R.id.btn_register)
+    Button btnRegister;
+    @BindView(R.id.tv_declare)
+    TextView tvDeclare;
+
+    @Override
+    protected void initTitle() {
+        tvTitle.setText("注册");
+        imgPre.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected void initData() {
+
+
+        //在最后一个Eidt上监听数据是否为空
+        btnRegister.setClickable(false);
+        etRegisterPwdagain.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            //text改变后
+            @Override
+            public void afterTextChanged(Editable s) {
+                //获取Edit上的数据
+                String name = etRegisterName.getText().toString().trim();
+                String number = etRegisterNumber.getText().toString().trim();
+                String pwd = etRegisterPwd.getText().toString().trim();
+                String pweAgain = etRegisterPwdagain.getText().toString().trim();
+
+                if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(number) && !TextUtils.isEmpty(pwd) &&
+                        !TextUtils.isEmpty(pweAgain)) {
+                    btnRegister.setClickable(true);
+                    btnRegister.setBackgroundResource(R.drawable.btn_home_selector);
+
+
+                } else {
+                    btnRegister.setClickable(false);
+                    btnRegister.setBackgroundResource(R.drawable.btn_bg_noclick);
+
+                }
+
+
+            }
+        });
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_register;
+    }
+
+
+    @OnClick({R.id.img_pre, R.id.btn_register, R.id.tv_declare})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.img_pre:
+                //销毁当前页
+                removeActivity(this);
+                break;
+            case R.id.btn_register:
+                //注册按钮
+                //获取Edit上的数据
+                String name = etRegisterName.getText().toString().trim();//中文编码乱码
+                String number = etRegisterNumber.getText().toString().trim();
+                String pwd = etRegisterPwd.getText().toString().trim();
+                String pweAgain = etRegisterPwdagain.getText().toString().trim();
+                if (TextUtils.isEmpty(name) || TextUtils.isEmpty(number) || TextUtils.isEmpty(pwd) ||
+                        TextUtils.isEmpty(pweAgain)) {
+                    Toast.makeText(this, "用户名密码不能为空", Toast.LENGTH_SHORT).show();
+
+                } else if (!pwd.equals(pweAgain)) {
+
+                    Toast.makeText(this, "两次密码不一致", Toast.LENGTH_SHORT).show();
+                    //密码置空
+                    etRegisterPwd.setText("");
+                    etRegisterPwdagain.setText("");
+                } else {
+                    //Toast.makeText(this, "成功", Toast.LENGTH_SHORT).show();
+                    //发送注册信息给服务器
+                    String url = AppNetConfig.USERREGISTER;
+                    RequestParams params = new RequestParams();//请求参数
+                    params.put("name", name);//根据接口文档封装请求参数
+                    params.put("password", pwd);
+                    params.put("phone", number);
+                    //post请求
+                    client.post(url, params, new AsyncHttpResponseHandler() {
+                        //联网成功后
+                        @Override
+                        public void onSuccess(int statusCode, String content) {
+
+                            //根据接口文档分析content中都封装了什么
+                            //服务器首先判断手机号是否被注册，并返回一个isExist 是否存在 true存在，false不存在
+                            //fastJSON解析content
+                            com.alibaba.fastjson.JSONObject jsonObject = com.alibaba.fastjson.JSONObject.parseObject(content);
+                            Boolean isExist = jsonObject.getBoolean("isExist");
+                            if (isExist){
+                                Toast.makeText(RegisterActivity.this, "手机号已存在", Toast.LENGTH_SHORT).show();
+
+                            }else{
+
+                                Toast.makeText(RegisterActivity.this, "注册成功", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        //联网失败后
+                        @Override
+                        public void onFailure(Throwable error, String content) {
+                            super.onFailure(error, content);
+                            Toast.makeText(RegisterActivity.this, "网络连接错误", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                }
+
+                break;
+            case R.id.tv_declare:
+                break;
+        }
+    }
+}
--- app/src/main/java/com/example/raito/p2pinvest/fragment/MoreFragment.java
+++ app/src/main/java/com/example/raito/p2pinvest/fragment/MoreFragment.java
@@ -1,16 +1,24 @@
 package com.example.raito.p2pinvest.fragment;
 
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.example.raito.p2pinvest.R;
+import com.example.raito.p2pinvest.activity.RegisterActivity;
+import com.example.raito.p2pinvest.common.BaseActivity;
 import com.example.raito.p2pinvest.common.BaseFragment;
 import com.loopj.android.http.RequestParams;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
 
@@ -26,6 +34,22 @@ public class MoreFragment extends BaseFragment {
     TextView tvTitle;
     @BindView(R.id.img_setting)
     ImageView imgSetting;
+    @BindView(R.id.tv_more_regist)
+    TextView tvMoreRegist;
+    @BindView(R.id.toggle_more)
+    ToggleButton toggleMore;
+    @BindView(R.id.tv_more_reset)
+    TextView tvMoreReset;
+    @BindView(R.id.tv_more_phone)
+    TextView tvMorePhone;
+    @BindView(R.id.rl_more_contact)
+    RelativeLayout rlMoreContact;
+    @BindView(R.id.tv_more_fankui)
+    TextView tvMoreFankui;
+    @BindView(R.id.tv_more_share)
+    TextView tvMoreShare;
+    @BindView(R.id.tv_more_about)
+    TextView tvMoreAbout;
     Unbinder unbinder;
 
     public MoreFragment() {
@@ -62,12 +86,39 @@ public class MoreFragment extends BaseFragment {
 
 
 
-    @OnClick({R.id.img_pre, R.id.img_setting})
+    @OnClick({R.id.img_pre, R.id.tv_title, R.id.img_setting, R.id.tv_more_regist, R.id.toggle_more, R.id.tv_more_reset, R.id.tv_more_phone, R.id.rl_more_contact, R.id.tv_more_fankui, R.id.tv_more_share, R.id.tv_more_about})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.img_pre:
                 break;
+            case R.id.tv_title:
+                break;
             case R.id.img_setting:
+                break;
+            case R.id.tv_more_regist:
+                //跳转到注册界面
+                ((BaseActivity)this.getActivity()).goToActivity(RegisterActivity.class,null);
+                break;
+            case R.id.toggle_more:
+                //开启手势密码
+                break;
+            case R.id.tv_more_reset:
+                //重置手势密码
+                break;
+            case R.id.tv_more_phone:
+
+                break;
+            case R.id.rl_more_contact:
+
+                break;
+            case R.id.tv_more_fankui:
+                //联系客服
+                break;
+            case R.id.tv_more_share:
+                //分享
+                break;
+            case R.id.tv_more_about:
+                //关于
                 break;
         }
     }
